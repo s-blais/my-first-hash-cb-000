55c21edab10f400c10a7a87ad4c55f3ef7f561ab
Stephen Blais <stephen@s-blais.com>
2019-08-23 14:55:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,6 +7,7 @@ def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
@@ -16,20 +17,14 @@ def retrieval
   shipping_manifest["oil paintings"]
 end
 
+
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
   shipping_manifest["muskets"] = 2
-
-  # add 4 gun powder to the shipping_manifest hash below
-
   shipping_manifest["gun powder"] = 4
-
-  # return the shipping_manifest hash below
   shipping_manifest
 end
